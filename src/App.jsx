515d30aabdae3e5eb063ea7555b14e95fdcfdaf6
Gero6019 <geronimo6019@gmail.com>
2025-03-04 19:55:08 -0300

    creando componente contenido

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import { Titulos } from './components/Titulos'
+import { Contenido } from './components/Contenido'
 
 /*
   Tarea 1: Crear un componente y mostrar 2 etiquetas html a eleccion.
+  Tarea 2: Crear un componente que se le pase un prop y mostrar por pantalla ese dato.
 */
 
 function App() {
@@ -11,7 +13,8 @@ function App() {
 
   return (
     <>
-    <Titulos />
+      <Titulos />
+      <Contenido nombreImagen={"flores"} />
     </>
   )
 }
---
+++ src/components/Contenido.jsx
@@ -0,0 +1,6 @@
+export const Contenido = ({ nombreImagen }) => {
+    return <>
+        <h3>{nombreImagen}</h3>
+        <img src="https://cdn0.ecologiaverde.com/es/posts/2/6/4/tulipanes_2462_1_600.webp" alt="" />
+    </>
+}
